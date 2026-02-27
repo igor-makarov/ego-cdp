1e88966e0ba80960bd2a61c7a8b82517a4de5a88
Igor Makarov <igormaka@gmail.com>
2026-02-27 15:40:32 +0200

    remove intentional failures

--- test/unexpected-fail1/command.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-printf 'this test should fail unexpectedly\n'
-exit 1
--- test/unexpected-fail2/command.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-printf 'another unexpected failure\n'
-false
